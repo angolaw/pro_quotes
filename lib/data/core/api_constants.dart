be9aeba65cf654d07266ae54bf72dadb94673c51
Willian S <wsapcf@gmail.com>
2022-01-05 11:35:59 -0300

    add PopularQuotesModel

--- lib/data/core/api_constants.dart
+++ lib/data/core/api_constants.dart
@@ -4,5 +4,5 @@ class ApiConstants {
   static const String API_RANDOM =
       "http://quotes.stormconsultancy.co.uk/random.json";
   static const String API_POPULAR =
-      "http://quotes.stormconsultancy.co.uk/popular.json)";
+      "http://quotes.stormconsultancy.co.uk/popular.json";
 }
---
+++ lib/data/models/popular_quotes_model.dart
@@ -0,0 +1,25 @@
+class PopularQuotesModel {
+  String? author;
+  int? id;
+  String? quote;
+  String? permalink;
+
+  PopularQuotesModel({this.author, this.id, this.quote, this.permalink});
+
+  factory PopularQuotesModel.fromJson(Map<String, dynamic> json) {
+    return PopularQuotesModel(
+        author: json['author'],
+        id: json['id'],
+        quote: json['quote'],
+        permalink: json['permalink']);
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = {};
+    data['author'] = author;
+    data['id'] = id;
+    data['quote'] = quote;
+    data['permalink'] = permalink;
+    return data;
+  }
+}
