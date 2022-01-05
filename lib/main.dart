5999909454e695c8610402116a790eeba2bc699d
Willian S <wsapcf@gmail.com>
2022-01-05 11:17:28 -0300

    make call to see if response is correct

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'package:pro_quotes/data/core/api_client.dart';
+import 'package:pro_quotes/data/datasources/random_quote_remote_datasource.dart';
+import 'package:pro_quotes/data/models/random_quote_model.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  RandomQuoteRemoteDatasource randomDataSource =
+      RandomQuoteRemoteDatasourceImpl(ApiClient(
+    Client(),
+  ));
+  final quote = await randomDataSource.getRandomQuote();
+  runApp(MyApp(
+    quote: quote,
+  ));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  final RandomQuoteModel quote;
+  const MyApp({Key? key, required this.quote}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -14,53 +26,12 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
+      home: Scaffold(
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+          children: [Center(child: Text(quote.quote ?? "No quote"))],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
