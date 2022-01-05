2a01cc1fa95c3d9374e30d7e7e4c5be54305b23b
Willian S <wsapcf@gmail.com>
2022-01-05 20:52:13 -0300

    add get test

--- test/data/datasources/quotes_remote_datasource_test.dart
+++ test/data/datasources/quotes_remote_datasource_test.dart
@@ -1,5 +1,7 @@
 // 1 - Criar mock do HttpClient
 // 2 - Criar impl do remoteDataSource
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
@@ -7,7 +9,7 @@ import 'package:pro_quotes/data/core/api_constants.dart';
 import 'package:pro_quotes/data/models/popular_quotes_model.dart';
 import 'package:pro_quotes/data/models/random_quote_model.dart';
 
-import '../../fixture/fixture_reader.dart';
+import '../../fixtures/fixture_reader.dart';
 
 class MockHttpClient extends Mock implements http.Client {}
 
@@ -38,25 +40,41 @@ void main() {
   late QuotesRemoteDatasourceImpl dataSource;
   late MockHttpClient mockHttpClient;
 
+  setUpAll(() {
+    registerFallbackValue(Uri.parse(ApiConstants.API_RANDOM));
+  });
+
   setUp(() {
     mockHttpClient = MockHttpClient();
     dataSource = QuotesRemoteDatasourceImpl(client: mockHttpClient);
   });
 
   group("getRandomQuote", () {
+    final referenceQuote =
+        RandomQuoteModel.fromJson(json.decode(fixture("quote.json")));
     test("should return a random quote when called", () async {
       //arrange
       // ! get uri with any null problem
-      when(mockHttpClient.get(any, headers: anyNamed("headers")))
+      when(() => mockHttpClient.get(any(), headers: any(named: "headers")))
           .thenAnswer((_) async => http.Response(fixture("quote.json"), 200));
 
       //act
       dataSource.getRandomQuote();
 
       //assert
-      verify(mockHttpClient.get(
-          Uri.parse("http://quotes.stormconsultancy.co.uk/quotes/1"),
-          headers: anyNamed("headers")));
+      verify(() => mockHttpClient.get(any(), headers: any(named: "headers")));
+    });
+    test("should perform a GET request to the correct URL with json headers ",
+        () async {
+      //arrange
+      when(() => mockHttpClient.get(any(), headers: any(named: "headers")))
+          .thenAnswer((_) async => http.Response(fixture("quote.json"), 200));
+
+      //act
+      final result = await dataSource.getRandomQuote();
+
+      //verify
+      expect(result, equals(referenceQuote));
     });
   });
 }
