db562bdfe552706d306fc241ea9b6e936fa00350
Willian S <wsapcf@gmail.com>
2022-01-05 10:53:59 -0300

    add api constants values

---
+++ lib/data/core/api_constants.dart
@@ -0,0 +1,8 @@
+class ApiConstants {
+  ApiConstants._();
+
+  static const String API_RANDOM =
+      "http://quotes.stormconsultancy.co.uk/random.json";
+  static const String API_POPULAR =
+      "http://quotes.stormconsultancy.co.uk/popular.json)";
+}
